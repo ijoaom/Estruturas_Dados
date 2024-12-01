2383f7f5b93ac2f2c6c7650f1d73f2022da95d0c
ijoaom <joaomanoel2020.1@gmail.com>
2024-11-30 23:35:55 -0300

    NoDuplo.kt

---
+++ ListaDinamica/NoDuplo.kt
@@ -0,0 +1,10 @@
+data class NoDuplo(val dado: Any?) {
+
+    var proximo: NoDuplo? = null
+    var anterior: NoDuplo? = null
+
+//data class e uma classe que nao tem regra de negocio e so serve pra armazenar dados, assim como //
+ //   a  classe pojo do java //
+
+}
+
